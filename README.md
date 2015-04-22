13a32fabf5aa081d9e9c423b5c04511a51a2c3ca
firefaith <firefaith@126.com>
2015-04-22 21:49:43 +0800

    v1.1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # wordcloud
+## condition
+node version: v0.10.32
+express version :4.12.1
+os : Ubuntu 14.04 
+## files
+views/index.ejs : 主页
+routers/nlp.js : 分词及统计逻辑
--- routes/nlp.js
+++ routes/nlp.js
@@ -2,21 +2,26 @@ var express = require('express');
 var router = express.Router();
 var nodejieba = require("nodejieba");
 nodejieba.loadDict("./node_modules/nodejieba/dict/jieba.dict.utf8", "./node_modules/nodejieba/dict/hmm_model.utf8", "./node_modules/nodejieba/dict/user.dict.utf8");
-
+var wc={
+  data : {},
+  add : function(key){
+         this.data[key]=this.data[key]==null?1:this.data[key]+1;
+        },
+  getData : function(){return this.data}
+}
 /* POST nlp string. */
 router.post('/', function(req, res) {
   console.log("in post nlp");
   console.log(req.body);
   var wordList = nodejieba.cutSync(req.body.content);
-  line='';
   if (wordList.constructor == Array) // just for tutorial, this is always be true 
   {
     wordList.forEach(function(word) {
-      line = line+','+word     
+      wc.add(word);     
     });
   }
-  console.log(line);
-  res.send(line);
+  console.log(wc.getData());
+  res.send(wc.getData());
   
 });
 
